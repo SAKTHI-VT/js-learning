9e92d40af15a135b136af4228ec444a778358674
SAKTHI-VT <sakthivelthirumoorthy2002@gmail.com>
2023-02-01 18:59:22 +0530

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//ALERT MESSAGE
+
+// let js='hello';
+// if(js==='hello') alert('hello');
+
+//MATHEMATICAL OPERATIONS
+
+// console.log(24+12-2); 
+
+//CONSOLE DISPLAY
+ 
+console.log("sakthi");
+console.log(14);
+
+//VALUES &VARIABLES
+
+let fsname="sakthi";
+console.log(fsname);
+console.log(fsname);
