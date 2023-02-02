484f8c094c1fbd48510927e2b9f6859579ff86e9
SAKTHI-VT <sakthivelthirumoorthy2002@gmail.com>
2023-02-02 12:12:55 +0530

    commit

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ console.log(14);
 
 //VALUES &VARIABLES
 
-let fsname="sakthi";
+let fsname="sakthi"; 
+let fs_name="SAKTHI";
 console.log(fsname);
-console.log(fsname);
+console.log(fs_name);
+
+//INCORRECT DECLARATION OF VARIABLE
+
+// let 3years=3;
+// let fs&name="sakthi";
