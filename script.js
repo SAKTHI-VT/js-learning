c8f990857aa880c02c99b2c5ac3148eddf801f49
SAKTHI-VT <sakthivelthirumoorthy2002@gmail.com>
2023-02-03 21:31:53 +0530

    commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 //MATHEMATICAL OPERATIONS
 
-console.log(24+12-2); 
+// console.log(24+12-2); 
 
 //CONSOLE DISPLAY
  
@@ -14,17 +14,17 @@ console.log("sakthi");
 
 //VALUES &VARIABLES
 
-let fsname="sakthi"; 
-let fs_name="SAKTHI";
-console.log(fsname);
-console.log(fs_name);
-let PI=3.145;
-console.log(PI);
-let myFirstjob='student';
-let myCurrentjob='developer';
+// let fsname="sakthi"; 
+// let fs_name="SAKTHI";
+// console.log(fsname);
+// console.log(fs_name);
+// let PI=3.145;
+// console.log(PI);
+// let myFirstjob='student';
+// let myCurrentjob='developer';
 
-console.log(myFirstjob);
-console.log(myCurrentjob);
+// console.log(myFirstjob);
+// console.log(myCurrentjob);
 
 //INCORRECT DECLARATION OF VARIABLE
 
@@ -36,26 +36,52 @@ console.log(myCurrentjob);
 //7 Primitive types
 
 // Number 
-let age=21;
+// let age=21;
 // String
-let name='sakthi';
+// let name='sakthi';
 // Boolean
-let fullage=true;
+// let fullage=true;
 // Undefined
 // Null
 // Symbol
 // BigInt
 
-let abc=true;
-console.log(abc);
+// let abc=true;
+// console.log(abc);
 // typeof to find the datatype
-console.log(typeof 'sakthi');
-console.log(typeof 23);
-console.log(typeof true);
-console.log(typeof 2.15);
+// console.log(typeof 'sakthi');
+// console.log(typeof 23);
+// console.log(typeof true);
+// console.log(typeof 2.15);
 
 // abc='y';
 // console.log(abc);
 
-let a=359/500*100;
-console.log(a);  
+// let a=359/500*100;
+// console.log(a);
+
+//DECLARING VARIABLES
+
+// let val=30;
+// val=21;
+
+// const b_year=2002;
+// b_year =2001;
+// const job;
+
+// var job='student';
+// job='developer';
+
+//DECLARING VARIABLE WITHOUT let KEYWORD
+
+// fname='sakthi';
+// console.log(fname);
+
+// OPERATORS
+
+const current = 2023;
+const sakthi = current-2002;
+const thiru = current-1973;
+console.log(sakthi,thiru);
+
+console.log(sakthi *2 , thiru/10, 2**3);
